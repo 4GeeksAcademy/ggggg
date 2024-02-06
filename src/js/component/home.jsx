5eb9d6a75e600ab473d7c181f32a9d74b0b932a8
nick <150211508+n1cksway@users.noreply.github.com>
2024-02-05 20:23:48 -0500

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// // import SecondsCounter from "./SecondsCounter";
+// //include images into your bundle
+
+
+// //create your first component
+// const Home = () => {
+// 	return (
+// 		<SecondsCounter/>
+// 	);
+// };
+
+// export default Home;
